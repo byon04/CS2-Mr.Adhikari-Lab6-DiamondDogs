887fa44f81f52ef04e3ec1cde30f08612bd4c1a9
DanielM <danieltmill8@gmail.com>
2026-02-27 20:16:32 -0500

    Added computer player that does a random interval

---
+++ ComputerPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class ComputerPLayer
+{
+	public ComputerPlayer()
+	{
+		// array for the values used by the computer
+		static string[] RandomRPS = { "rock", "paper", "scissors" };
+
+		// an instance for the generator
+		static RandomGenerator random = new RandomGenerator();
+
+	//Method to get the choice
+	public static string ComputerChoice()
+	{
+		//random generator
+		int pickRandom = random.Next(RandomRPS.Length);
+		// generator return
+		return RandomRPS[pickRandom];
+	}
+
+	//test
+	static void Main(string[] args)
+	{
+		string computerMove = ComputerChoice();
+	}
+	}
+}
